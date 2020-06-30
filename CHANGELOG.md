df64e87b2a7d71115fe49b3f5d83e42e2f35bf7d
Kyle Sunden <git@ksunden.space>
2020-06-30 15:20:05 +0000

    Avro

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/).
 
 ## [Unreleased]
 
+### Changed
+- Update to avro-backed yaqd-core [YEP-107](https://yeps.yaq.fyi/107)
+- Update to flit for packaging
+
 ## [2020.05.0]
 
 ### Added
--- yaqd_seabreeze/_seabreeze.py
+++ yaqd_seabreeze/_seabreeze.py
@@ -6,14 +6,9 @@ from typing import Dict, Any, List
 from seabreeze.spectrometers import Spectrometer  # type: ignore
 from yaqd_core import Sensor, logging
 
-from .__version__ import __branch__
-
 
 class Seabreeze(Sensor):
     _kind = "seabreeze"
-    _version = "0.1.0" + f"+{__branch__}" if __branch__ else ""
-    traits: List[str] = ["is-sensor"]
-    defaults: Dict[str, Any] = {}
 
     def __init__(self, name, config, config_filepath):
         self.spec = Spectrometer.from_serial_number(config.get("serial"))
@@ -23,31 +18,14 @@ class Seabreeze(Sensor):
         self._correct_dark_counts = config.get("correct_dark_counts", False)
         self._correct_nonlinearity = config.get("correct_nonlinearity", False)
 
-        self.channel_names = ["wavelengths", "intensities"]
-        self.channel_units = {"wavelengths": "nm", "intensities": None}
-        self.channel_shapes = {
+        self._channel_names = ["wavelengths", "intensities"]
+        self._channel_units = {"wavelengths": "nm", "intensities": None}
+        self._channel_shapes = {
             "wavelengths": (self.spec.pixels,),
             "intensities": (self.spec.pixels,),
         }
 
-    def _load_state(self, state):
-        """Load an initial state from a dictionary (typically read from the state.toml file).
-
-        Must be tolerant of missing fields, including entirely empty initial states.
-
-        Parameters
-        ----------
-        state: dict
-            The saved state to load.
-        """
-        super()._load_state(state)
-        self._integration_time_micros = state.get("integration_time_micros", 3000)
-        self.spec.integration_time_micros(self._integration_time_micros)
-
-    def get_state(self):
-        state = super().get_state()
-        state["integration_time_micros"] = self._integration_time_micros
-        return state
+        self.set_integration_time_micros(self._state["integration_time_micros"])
 
     async def _measure(self):
         return {
