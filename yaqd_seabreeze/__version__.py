cf631a0451b117a0a9063d17a25e3b80d6b8c79f
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2025-04-10 11:36:44 -0500

    fix test_pypi publish clause

--- yaqd_seabreeze/__version__.py
+++ yaqd_seabreeze/__version__.py
@@ -24,6 +24,6 @@ try:
 except:
     __branch__ = ""
 
-publishing = os.getenv("no_local_version") == "yaqd_seabreeze"
+publishing = os.getenv("publishing") == "yaqd_seabreeze"
 if __branch__ and not publishing:
     __version__ += "+" + __branch__
